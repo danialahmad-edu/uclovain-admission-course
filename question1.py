4672ccddba0bbc648708415e4f64bdaa42a58a9c
Danial Ahmad <108906119+iamdanialahmad@users.noreply.github.com>
2024-04-29 04:26:05 +0500

    Refactor question1.py to remove unnecessary code

--- question1.py
+++ question1.py
@@ -24,13 +24,3 @@ def mat_to_list(adj_mat):
     return adj_list
 
 
-adj_mat = [[0, 1, 0, 1, 0, 0],
-           [0, 0, 1, 0, 0, 0],
-           [1, 0, 0, 0, 0, 0],
-           [0, 0, 0, 0, 1, 0],
-           [0, 0, 0, 1, 0, 0],
-           [0, 0, 0, 0, 0, 0]]
-
-
-print(mat_to_list(adj_mat))
-
