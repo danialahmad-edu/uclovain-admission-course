968c47e589e5a3ef20a55260035c4d8763a305f3
Danial Ahmad <108906119+iamdanialahmad@users.noreply.github.com>
2024-04-29 04:25:00 +0500

    Add function to convert adjacency matrix to adjacency list

---
+++ question1.py
@@ -0,0 +1,36 @@
+def mat_to_list(adj_mat):
+    """ Convert adjacency matrix to an adjacency list representation
+
+    Parameters:
+    -----------
+    adj_mat : (a square 0-1 matrix)
+        Adjacency matrix (n x n) of the graph (of n nodes)
+
+
+    Returns:
+    --------
+     adj_list: `list[list[int]]`
+        The adjacency list of the graph
+
+    """
+    adj_list = []
+    
+    for i in range(len(adj_mat)):
+        adj_list.append([])
+        for j in range(len(adj_mat[i])):
+            if adj_mat[i][j] == 1:
+                adj_list[i].append(j)
+    
+    return adj_list
+
+
+adj_mat = [[0, 1, 0, 1, 0, 0],
+           [0, 0, 1, 0, 0, 0],
+           [1, 0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 1, 0],
+           [0, 0, 0, 1, 0, 0],
+           [0, 0, 0, 0, 0, 0]]
+
+
+print(mat_to_list(adj_mat))
+
