32e8534e347766eaa446c81df94fa5a89e5190c5
Danial Ahmad <108906119+iamdanialahmad@users.noreply.github.com>
2024-04-29 04:46:24 +0500

    Add unit tests for mat_to_list and reachable functions

---
+++ main.py
@@ -0,0 +1,47 @@
+import unittest
+from question1 import mat_to_list
+from question2 import reachable
+
+
+class TestMatToList(unittest.TestCase):
+    def test_empty_matrix(self):
+        self.assertEqual(mat_to_list([]), [])
+
+    def test_single_node(self):
+        self.assertEqual(mat_to_list([[0]]), [[]])
+
+    def test_two_nodes_not_connected(self):
+        self.assertEqual(mat_to_list([[0, 0], [0, 0]]), [[], []])
+
+    def test_two_nodes_connected(self):
+        self.assertEqual(mat_to_list([[0, 1], [0, 0]]), [[1], []])
+
+    def test_multiple_nodes(self):
+        adj_mat = [[0, 1, 0, 1, 0, 0],
+                   [0, 0, 1, 0, 0, 0],
+                   [1, 0, 0, 0, 0, 0],
+                   [0, 0, 0, 0, 1, 0],
+                   [0, 0, 0, 1, 0, 0],
+                   [0, 0, 0, 0, 0, 0]]
+        self.assertEqual(mat_to_list(adj_mat), [[1, 3], [2], [0], [4], [3], []])
+
+class TestReachable(unittest.TestCase):
+    def test_empty_graph(self):
+        self.assertEqual(reachable([], 0), set())
+
+    def test_single_node(self):
+        self.assertEqual(reachable([[]], 0), {0})
+
+    def test_two_nodes_not_connected(self):
+        self.assertEqual(reachable([[], []], 0), {0})
+
+    def test_two_nodes_connected(self):
+        self.assertEqual(reachable([[1], []], 0), {0, 1})
+
+    def test_multiple_nodes(self):
+        adj_list = [[1, 3], [2], [0], [4], [3], []]
+        self.assertEqual(reachable(adj_list, 0), {0, 1, 2, 3, 4})
+        self.assertEqual(reachable(adj_list, 3), {3, 4})
+
+if __name__ == '__main__':
+    unittest.main()
